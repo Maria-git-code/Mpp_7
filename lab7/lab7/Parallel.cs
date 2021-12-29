7b89f343ea6695f73fea55008785ba34fadaec86
Maria-git-code <83037470+Maria-git-code@users.noreply.github.com>
2021-12-29 15:51:46 +0300

    Add files via upload

---
+++ lab7/lab7/Parallel.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+
+namespace lab7
+{
+    public class Parallel
+    {
+        public static void WaitAll(WaitCallback[] tasks)
+        {
+            foreach (var task in tasks)
+            {
+                ThreadPool.QueueUserWorkItem(task);
+            }
+
+            bool readyToStop;
+            do
+            {
+                ThreadPool.GetMaxThreads(out int mWorkers, out int mIoThreads);
+                ThreadPool.GetAvailableThreads(out int aWorkers, out int aIoThreads);
+                readyToStop = mWorkers == aWorkers;
+            } while (!readyToStop);
+        }
+    }
+}
